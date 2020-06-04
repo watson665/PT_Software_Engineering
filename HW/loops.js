50919a2fc9ab7acd8c885d88fec6e2f653c940b0
PSCBUS_107 <pscbus_107@PSCBUS-107.local>
2020-06-04 11:00:43 -0400

    finished loop practice

--- HW/loops.js
+++ HW/loops.js
@@ -1,9 +1,55 @@
-/*for(a = 0; a <= 99; a++) {
+//Loop 0-99 Ascending
+for(a = 0; a <= 99; a++) {
     console.log(a);
-};*/
+};
 
+//Loop 99-0 Descending
 for(b = 99; b >= 0; b--){
-    console.log(b)
+    console.log(b);
+};
+
+//Loop 0-98 Even
+let c= 0
+for(c = 0; c <= 98; c++) {
+    if(c % 2 == 0);
+    console.log(c);
+}; 
+
+//Loop 90-0 Even
+let d = 98;
+for(d = 98; d >= 0; d--){
+    if(d % 2 == 0);
+    console.log(d);
+};
+
+//Loop 0-99 Odd
+let e = 0;
+for(e = 0; e <= 99; e++) {
+    if(e % 2 !== 0);
+    console.log(e);
+};
+
+//Loop 99-0 Odd
+let g = 99;
+for(g = 99; g >= 0; g--){
+    if(g % 2 !== 0);
+    console.log(g);
+};
+
+//Loop 49-72 Ascending
+let h = 49;
+for(h = 49; h <= 72; h++) {
+    console.log(h);
+};
+
+//Loop 81-26 Descending
+let i = 81;
+for(i = 81; i >= 26; i--) {
+    console.log(i)
 }
 
-
+//Loop 3-90 Multiples of 3 Ascending
+let j = 3;
+for(j = 3; j <= 90; j+=3){
+    console.log(j)
+}
