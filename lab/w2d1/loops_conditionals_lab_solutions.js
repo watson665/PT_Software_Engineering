6496ee947140a048f910fb2fa88d70c3bed24172
PSCBUS_107 <pscbus_107@PSCBUS-107.local>
2020-06-07 16:34:48 -0400

    1sr half of w2d1 lab

---
+++ lab/w2d1/loops_conditionals_lab_solutions.js
@@ -0,0 +1,94 @@
+/*
+Rate your understanding of the following questions on a scale of 1 - 5. If your understanding is less than or equal to 3, research the answer or ask for help:
+
+1. How do we assign a value to a variable? var, const, let = (insert variable here)
+Which line(s) is/are valid?
+let a = 0; ----> valid
+const b = 1; ----> valid
+9 = a; ----> invalid
+b = b + 5; ----> invalid
+
+How do we change the value of a variable? write the variable name, followed by = , followed by your new value.
+How do we assign an existing variable to a new variable? using the function method
+*/
+/*
+// Strings Activity 1 - Switcharoo
+let firstVariable = 'Hello World';
+console.log(firstVariable);
+
+firstVariable = 7;
+console.log(firstVariable);
+
+secondVariable = firstVariable;
+console.log(secondVariable);
+
+secondVariable = 'firstVariable';
+console.log(firstVariable);
+console.log(secondVariable);
+//What is the value of firstVariable? 7
+
+//Strings - Comnbine it
+let yourName = 'DeAmber';
+console.log('Hello, my name is ' + yourName)
+
+
+//Booleans Activity
+const a = 6;
+const b = 100;
+const c = -5;
+const d = 3000;
+const e = 'Jelly Bean';
+
+console.log(a < 100)
+console.log(c < d)
+console.log('Peanut' === 'Peanut')
+console.log(a < b > c)
+console.log(a <= a < d)
+console.log(e === 'Jelly Bean')
+console.log(48 !== '48')
+console.log('e' !== 'Eh')
+
+//The Farm
+let animal = 'dog';
+if (animal === 'cow') {
+    console.log('mooooo')
+} else {
+    console.log('Hey! You\'re not a cow.')
+}
+
+//Driver's Ed
+let personsAge = 14
+if (personsAge >= 16) {
+    console.log('Here are the keys!')
+} else {
+    console.log('Sorry, you\'re too young')
+}
+
+//Just Loop It
+for(i = 0; i <= 10; i++)
+console.log(i)
+
+for(j = 500; j >= 50; j--)
+console.log(j)
+
+//Let's get odd
+for(k = 1; k <= 100; k++)
+if (k % 2 !== 0) {
+    console.log(k + ' is an odd number')
+}
+
+//Give me Five
+for(j = 0; j <= 100; j++)
+if (j % 5 === 0) {
+    console.log('I found a ' + j + '. High five!')
+} else {
+    (j % 3 === 0)
+    console.log('I found a ' + j + '. Three is a crowd!')
+}
+*/
+//Savings Account
+
+            
+       
+
+
