38c6e90168bb313a35fc42c42c079e84ccb9afdb
PSCBUS_107 <pscbus_107@PSCBUS-107.local>
2020-06-07 13:07:45 -0400

    lab 1 complete

---
+++ lab/w1d3/loops_and_boolean_practice.js
@@ -0,0 +1,31 @@
+//Create a loop that will print "hi" 15 times.
+for(let i = 0; i < 15; i++)
+    console.log('hi ');
+
+//Create a loop that will print "YEP!" 30 times.
+for(let j = 0; j < 30; j++)
+    console.log('YEP!');
+
+//Booleans & Loops - Part 1
+//Create a variable called some_num and assign a random number.
+const sum_num = 20;
+//Create a loop that will run ten times
+for(let k = 0; k < 10; k++)
+    console.log(sum_num)
+//Inside the loop, print "that's a small number" if some_num is less than 10.
+    if(sum_num < 10){
+        console.log('that\'s a small number')
+    } 
+    else {
+        (sum_num > 20)
+        console.log('that\'s a big number')
+    }
+
+//Booleans & Loops - Part 2
+let fave_day = 'monday';
+for(i = 0; i < 1; i++)
+if (fave_day === 'saturday' || 'sunday') {
+    console.log('I like the weekend')
+} 
+else (fave_day === 'monday' || 'tuesday' || 'wednesday' || 'thursday' || 'friday')
+    console.log('Give me a good \'ol weekday')
