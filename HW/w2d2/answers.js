1e194762c03f6bdf6ff1e32809d2e349a04d898f
PSCBUS_107 <pscbus_107@PSCBUS-107.local>
2020-06-07 22:51:35 -0400

    Get Even answered

--- HW/w2d2/answers.js
+++ HW/w2d2/answers.js
@@ -1,3 +1,9 @@
 //Write a for loop that will log the numbers 1 through 20
 for(i = 1; i <= 20; i++)
 console.log(i)
+
+//Write a for loop that will log only the even numbers in 0 through 200
+for(i = 0; i <= 200; i++) {
+   if (i % 2 === 0)
+    console.log(i)
+}
