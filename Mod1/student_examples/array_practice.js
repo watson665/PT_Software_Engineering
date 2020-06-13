25f02a450f7b61ab92a499ef8e2c62598807566f
PSCBUS_107 <pscbus_107@PSCBUS-107.local>
2020-06-13 19:34:37 -0400

    more practice

---
+++ Mod1/student_examples/array_practice.js
@@ -0,0 +1,109 @@
+/*
+const arr = [];
+console.log(arr)
+
+const num = 0;
+const str = 'strings are cool';
+const bool = true;
+const arr = [];
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof arr);
+console.log(Array.isArray(arr));
+
+const faveFoods = ['pizza', 'chicken', 'icecream'];
+console.log(faveFoods);
+console.log(faveFoods[0]);
+console.log(faveFoods[1]);
+console.log(faveFoods[2]);
+
+const ghostBusters = ['Venkman', 'Spengler', 'Stantz', 'Zeddemore', 'Melnitz', 'Barrett', 'Tully'];
+console.log(ghostBusters[0]);
+console.log(ghostBusters[6]);
+console.log(ghostBusters.length);
+console.log(ghostBusters[4-1]);
+
+const veggies = ['red pepper', 'leek', 'pumpkin', 'cauliflower'];
+veggies[1] = 'spinach';
+console.log(veggies);
+
+const numbers = [21, 18, 5, 3, 2, 1, 1];
+numbers[2] = null;
+console.log(numbers);
+numbers[0] *= 10;
+console.log(numbers);
+
+const list = ['cahir', 'table', 'candle', 'map', 'magnifying glass', 'rupees'];
+console.log(list[2 * 2]); 
+console.log(list[list.length - 1]);
+
+const shortArray = ['first', 'middle', 'last'];
+const index = 1 + 1;
+console.log(shortArray[index]);
+
+const kitchenSink = ['Dirty spoon', 'sponge', 'plate with gunk', 'soap', 'water'];
+for (let i = 0; i < kitchenSink.length; i++) {
+    console.log(`This is in my sink: ${kitchenSink[i]}.`);
+}
+
+for (let i = 0; i < kitchenSink.length; i += 2) {
+    console.log(`This is in my sink: ${kitchenSink[i]}.`);
+}
+
+const drSeuss = ['Cat in the Hat', 'Green Eggs and Ham', 'Grinch', 'Thing One', 'Thing Two', 'Cindy Loo Who', 'Jojo McDodd'];
+console.log(drSeuss);
+
+for (i = 0; i < drSeuss.length; i++) {
+    if (i % 2 !== 0) {
+        console.log(drSeuss[i])
+    }
+}
+
+for (i = 0; i < drSeuss.length; i++) {
+    if (i % 2 !== 0) {
+        console.log(drSeuss[i], i)
+    }
+}
+
+const foodSelection = ['pizza', 'apple', 'seaweed', 'artichoke','tea', 'ice cream', 'sushi', 'tacos'];
+
+for (let i = 0; i < foodSelection.length; i++) {
+    if ((i === 5) || (i % 2 === 0)) {
+        console.log(foodSelection[i]);
+    }
+}
+
+for (let i = 0; i < foodSelection.length; i++) {
+    if (i % 2 === 0 && i % 3 === 0) {
+        console.log(foodSelection[i]);
+    }
+}
+
+const looneyTunesChars = ['Bugs Bunny', 'Daffy Duck', 'Tweety', 'Sylvester', 'Elmer Fudd', 'Porky Pig', 'Taz'];
+
+for (let i = 0; i < looneyTunesChars.length; i++) {
+    if (i % 2 === 0) {
+        console.log(looneyTunesChars[i])
+    }
+}
+
+for (let i = 0; i < looneyTunesChars.length; i++) {
+    if ((i % 2 === 0) && (i % 3 === 0)) {
+        console.log(looneyTunesChars[i]);
+    }    
+}
+
+for (let i = 0; i < looneyTunesChars.length; i++) {
+    if (i % 2 !== 0) {
+        console.log(looneyTunesChars[i])
+    }
+}
+*/
+const pairs = [['Snoopy', 'Linus'], ['Peppermint Patty', 'Woodstock']];
+console.log(pairs.length);
+console.log(pairs[0][0]);
+console.log(pairs[0][1]);
+console.log(pairs[1][0]);
+console.log(pairs[1][1]);
