16544e509b4fb0729c8fb6ff7c83b70df24b6730
PSCBUS_107 <pscbus_107@PSCBUS-107.local>
2020-06-15 16:01:56 -0400

    datatype refresher complete

---
+++ HW/w3d3/answers.js
@@ -0,0 +1,68 @@
+//For each of the following, write which datatypes you would use to represent the data, and then give a small example of the data structure:
+
+//1. A light switch that can be either on or off.
+//Datatype: Boolean
+/*Data Structure:*/ const lightSwitch = true;
+
+//2. A user's email address.
+//Datatype: String
+/*Data Structure:*/ const userEmail = 'deamberwatson@gmail.com';
+
+//3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+//Datatype: Array & String
+/*Data Structure:*/ const spaceship = ['hull', 'laser blaster', 'tractor beam', 'warp drive'];
+
+//4. A list of student names from our class.
+//Datatype: Array & String
+/*Data Structure:*/ const studentNames = ['DeAmber', 'Kimmy', 'Beth', 'Hector'];
+
+//5. A list of student names from our class, each with a location.
+//Datatype: Object & String
+//Data Structure: 
+const student1 = {
+    name1: 'DeAmber',
+    Location1: 'Ohio'
+}
+const student2 = {
+    name2: 'Kimmy',
+    location2: 'Ohio'
+}
+const student3 = {
+    name3: 'Beth',
+    location3: 'Ohio'
+}
+const student4 = {
+    name4: 'Hector',
+    location4: 'Ohio'
+}
+
+//6. A list of student names from our class, each with a location and each with a list of favorite tv shows.
+//Datatype: Object & Array & String
+//Data Structure:
+const student1 = {
+    name1: 'DeAmber',
+    Location1: 'Ohio',
+    favShows1: ['Even Stevens', 'Lizzie McGuire', 'That\'s so Raven']
+}
+const student2 = {
+    name2: 'Kimmy',
+    location2: 'Ohio',
+    favShows2: ['The Proud Family', 'Kim Possible', 'Phil of the Future']
+}
+const student3 = {
+    name3: 'Beth',
+    location3: 'Ohio',
+    favShows3: ['Wizards if Waverly Place', 'The Suite Life of Zack & Cody', 'Hannah Montana']
+}
+const student4 = {
+    name4: 'Hector',
+    location4: 'Ohio',
+    favShows4: ['American Dragon: Jake Long', 'Cory in the House', 'Phineas and Ferb']
+}
+
+
+
+
+
+
+
