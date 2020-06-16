811b41ffc0f20b8b78ca7f58003908e11f449dfe
PSCBUS_107 <pscbus_107@PSCBUS-107.local>
2020-06-15 21:09:44 -0400

    crazy object complete

--- HW/w3d3/answers.js
+++ HW/w3d3/answers.js
@@ -56,3 +56,47 @@ const deamber = {
 //4. Write code that will access your hobby from the object that you just created.
 deamber.hobby;
 console.log(deamber.hobby);
+
+
+// Crazy Object!
+
+const crazyObject = {
+    taco: [{meat: 'steak',
+           cheese: ['panela', 'queso', 'chihuahua']},
+           {meat: 'chicken',
+            salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+          ],
+    larry: {
+      nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+      quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+      characters: [{
+        name: "Jeff",
+        occupation: "manager"
+      },
+      {
+        name: "funkhauser",
+        occupation: "tv dude"
+      },
+      {
+        name: "susie",
+        occupation: "jeffs wife",
+        favourtieHobby: "Swearing at Larry and Jeff"
+      },
+      ]
+    }
+  }
+
+//Use crazy Object to log the following.
+//1. "omg my mouth is burning"
+console.log(crazyObject.taco[1].salsa[5]);
+//2. "Pretty pretty prettayyyyy good"
+console.log(crazyObject.larry.quotes[0]);
+//3. "Swearing at Larry and Jeff"
+console.log(crazyObject.larry.characters[2].favourtieHobby);
+//4. Chicken Teriyaki Boyyyyyy"
+console.log(crazyObject.larry.nicknames[1]);
+//5. The object the contains the name funkhauser
+console.log(crazyObject.larry.characters[1].name);
+//6. Add the quote "I'm trying to elevate small talk to medium talk" to Larry's quote array. Confirm that it was added.
+crazyObject.larry.quotes.push('I\'m trying to elevate small talk to medium talk');
+console.log(crazyObject.larry.quotes)
