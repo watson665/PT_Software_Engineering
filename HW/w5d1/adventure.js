2d80ffc0835a2f3d2c960d7fde683db35f548b06
PSCBUS_107 <pscbus_107@PSCBUS-107.local>
2020-06-28 18:31:45 -0400

    created our enemy pizza rat

--- HW/w5d1/adventure.js
+++ HW/w5d1/adventure.js
@@ -1,3 +1,5 @@
+//Our Hero
+
 class Hero {
     constructor(name) {
         this.name = name;
@@ -22,3 +24,31 @@ class Hero {
 }
 
 const Dougie = new Hero('Dougie');
+
+//Our Enemy
+
+class Enemy {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.weapons = {pepperoniStars: 10, cheeseGrease: 10};
+        this.catchPhrase = ['I\'m youtube famous', 'I\'m more dangerous than an uncovered sewer'];
+    }
+    talkSmack() {
+        let phrases = this.catchPhrase;
+        let random = Math.random();
+        let totalPhrase = this.catchPhrase.length;
+        let randomIndex = Math.floor(random * totalPhrase);
+        let randomPhrase = phrases[randomIndex];
+        return randomPhrase;    
+    }
+    announceHealth() {
+        return this.health;
+    }
+    fight() {
+        return 'I\'m gonna flatten you like a slice of pepperoni'
+    }
+}
+
+const PizzaRat = new Enemy('Pizza Rat');
+
