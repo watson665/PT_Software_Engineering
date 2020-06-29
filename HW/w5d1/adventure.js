fbc84b4d7d6dd7f6913a802465616a375b45fcd0
PSCBUS_107 <pscbus_107@PSCBUS-107.local>
2020-06-28 21:51:20 -0400

    dougie and pizza rat have fought

--- HW/w5d1/adventure.js
+++ HW/w5d1/adventure.js
@@ -16,13 +16,20 @@ class Hero {
         return randomPhrase;
     }
     announceHealth() {
-        return this.health;
+        return `${this.name}'s health is ${this.health}.`;
     }  
-    fight() {
-        return 'I\'m ready to rumble.'
-    }
+        fight(enemy) {
+            //console.log('i\'m ready to rumble');
+            let random = Math.round(Math.random());
+            if (random == 1) {
+                enemy.health -= this.weapons.sugarShock;
+                return (`${this.name} has used sugar shock`)
+            } else {
+                enemy.health -= this.weapons.sprinkleSpray;
+                return (`${this.name} has used sprinkle spray`)
+            }
+        }
 }
-
 const Dougie = new Hero('Dougie');
 
 //Our Enemy
@@ -31,7 +38,7 @@ class Enemy {
     constructor(name) {
         this.name = name;
         this.health = 100;
-        this.weapons = {pepperoniStars: 10, cheeseGrease: 10};
+        this.weapons = {pepperoniStars: 5, cheeseGrease: 10};
         this.catchPhrase = ['I\'m youtube famous', 'I\'m more dangerous than an uncovered sewer'];
     }
     talkSmack() {
@@ -43,12 +50,32 @@ class Enemy {
         return randomPhrase;    
     }
     announceHealth() {
-        return this.health;
+        return `${this.name}'s health is ${this.health}.`;
     }
-    fight() {
-        return 'I\'m gonna flatten you like a slice of pepperoni'
-    }
+    fight(hero) {
+        //return 'I\'m gonna flatten you like a slice of pepperoni'
+            let random = Math.round(Math.random());
+            if (random == 1) {
+                hero.health -= this.weapons.pepperoniStars;
+                return (`${this.name} has used pepperoni stars`)
+            } else {
+                hero.health -= this.weapons.cheeseGrease;
+                return (`${this.name} has used cheese grease`)
+            }
+        }
 }
 
 const PizzaRat = new Enemy('Pizza Rat');
+
+//Walking Down the Street 
+console.log(Dougie.talkSass());
+console.log(PizzaRat.talkSmack());
+console.log(Dougie.announceHealth());
+console.log(PizzaRat.announceHealth());
+
+//Fight!
+console.log(Dougie.fight(PizzaRat));
+console.log(PizzaRat.fight(Dougie));
+console.log(PizzaRat.announceHealth());
+console.log(Dougie.announceHealth());
 
