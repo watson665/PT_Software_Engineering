ff2303287f57f782557bcfb9681ed75dc42bc221
PSCBUS_107 <pscbus_107@PSCBUS-107.local>
2020-07-02 18:53:10 -0400

    I added my first element and it was fun

--- HW/w5d3/hogwarts/app.js
+++ HW/w5d3/hogwarts/app.js
@@ -1,3 +1,46 @@
 /*if (typeof jQuery == 'undefined'){
     console.log('oops! I still have to link my jQuery properly!');
   } else {console.log('I did it! I linked jQuery and this js file!')}; */
+
+  ///////////////////////
+  /////// Year 1 ///////
+  /////////////////////
+
+  $(() => {
+    //Query for your div with the id of container and set it to a variable named $container
+    const $container = $('#container');
+    //console.log $container
+    console.log($container);
+    //Create an <h1> element and set it to a variable called $h1 and console log it
+    const $h1 = $('<h1>');
+    $('body').append($h1)
+    console.log($h1);
+    //Add some text inside the h1 element. Example text: 'Hogwarts'
+    $('h1').text('Hogwarts');
+    //Test yourself
+    console.log(document);
+});
+
+///////////////////////
+/////// Year 2 ///////
+/////////////////////
+
+///////////////////////
+/////// Year 3 ///////
+/////////////////////
+
+///////////////////////
+/////// Year 4 ///////
+/////////////////////
+
+///////////////////////
+/////// Year 5 ///////
+/////////////////////
+
+///////////////////////
+/////// Year 6 ///////
+/////////////////////
+
+///////////////////////
+/////// Year 7 ///////
+/////////////////////
