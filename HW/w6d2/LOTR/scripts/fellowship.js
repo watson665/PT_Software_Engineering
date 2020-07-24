c9fef758ae0482ea6c3752e1f5b525c8041b3930
PSCBUS_107 <pscbus_107@PSCBUS-107.local>
2020-07-24 14:35:06 -0400

    chap 1 & 2 js query complete

--- HW/w6d2/LOTR/scripts/fellowship.js
+++ HW/w6d2/LOTR/scripts/fellowship.js
@@ -36,15 +36,14 @@ const lands = ['The-Shire', 'Rivendell', 'Mordor'];
 // Chapter 1
 // ============
 const makeMiddleEarth = () => {
-
   // HINT: Make a console.log for each of your functions to make sure that, when you click, the correct function is being called!
-  console.log('making Middle Earth');
+  console.log("making Middle Earth");
   // console.log("Trying to make middle earth.");
-
+  console.log("Trying to make middle earth.");
   // 1. create a section tag with an id of middle-earth
-
+  const $section = $("<section>").attr("id", "middle-earth");
   // 2. append the section to the body of the DOM.
-
+  $("body").append($section);
   // 3. use a for loop to iterate over the lands array that does the following:
 
   //   3a. creates an article tag (there should be one for each land when the loop is done)
@@ -54,7 +53,13 @@ const makeMiddleEarth = () => {
   //   3c. includes an h1 with the name of the land inside each land article
 
   //   3d. appends each land to the middle-earth section
-
+  for (let i = 0; i < lands.length; i++) {
+    //console.log(lands[i]);
+    const $lands = $("<article>").attr("id", lands[i]);
+    const $h1 = $("<h1>").text(lands[i]);
+    $section.append($lands);
+    $lands.append($h1);
+  }
 };
 
 // COMMIT YOUR WORK
@@ -64,19 +69,20 @@ const makeMiddleEarth = () => {
 // Chapter 2
 // ============
 const makeHobbits = () => {
-
   // Goal: display an unordered list of hobbits in the shire (which is the first article tag on the page)
 
   // 1. create a 'ul'
-
+  const $hobbitUl = $("<ul>");
   // 2. make each hobbit an li element and append it to the 'ul' you just created
-    // hint: use the given 'hobbits' array and use a for loop
-
+  // hint: use the given 'hobbits' array and use a for loop
+  for (let i = 0; i < hobbits.length; i++) {
+    const $li = $("<li>").text(hobbits[i]).addClass("hobbit");
+    $hobbitUl.append($li);
+  }
   // 3. also, give each hobbit (`li`) a class of "hobbit"
-
   // 4. append the ul to the shire
-    // hint: get 'The-Shire' by using its id
-
+  // hint: get 'The-Shire' by using its id
+  $("#The-Shire").append($hobbitUl);
 };
 
 // COMMIT YOUR WORK
