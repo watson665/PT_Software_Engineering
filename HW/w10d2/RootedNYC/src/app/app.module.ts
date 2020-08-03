8a682263044d4f185cd815f69a9ce7ebbc7eef21
PSCBUS_107 <pscbus_107@PSCBUS-107.local>
2020-08-03 08:21:55 -0400

    not perfect but final turning this in

--- HW/w10d2/RootedNYC/src/app/app.module.ts
+++ HW/w10d2/RootedNYC/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AnnouncementComponent } from './announcement/announcement.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    AnnouncementComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
