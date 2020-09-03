8dba85d473c56c42b2d97bf1fc0e3b407649c0b7
PSCBUS_107 <pscbus_107@PSCBUS-107.local>
2020-09-03 17:47:16 -0400

    hw complete

---
+++ HW/theoretical_learning/app.js
@@ -0,0 +1,31 @@
+/*
+
+PT Assignments Theoretical Learning!!!
+
+Take some notes on these concepts and submit the file with the notes however you wish
+
+1) Inversion of Control
+IoC is a principal of Software Engineering. 
+IoC enables a framework to take control of the flow of a program.
+In IoC, the control of the objects or a protion of a program is transferred to a container or framework.
+To enable IoC, Frameworks use abstractions.
+The Spring Container is the core of the Spring Framework. 
+The container will create the objects, wire them together, configure them, and manage their complete life cycle.
+Objects in Spring are called Spring Beans. 
+
+2) Dependency Injection
+Can be done by setter injection or constructor injection.
+
+3) The MVC pattern
+Model View Controller Framework is an architecture. 
+Separates the application in to three logic components, Model - View - Controller.
+Model: includes all the data and its related logic.
+View: Presents data to the user or handles user interaction.
+Controller: interface between Model and View components.
+
+4) Spring Profiles
+A tool in Spring for grouping configuration properties.
+Default profile is always active.
+Other profiles will be evaluated on top of the default profile.
+
+*/
